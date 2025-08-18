c1fe7bc70088842a3cbe48aeb3d0ac25db506ccf
RobGroenestein <rob@groenestein.eu>
2025-08-18 16:16:23 +0200

    Update sensor.py

--- esphome/components/jk_bms_multi/sensor.py
+++ esphome/components/jk_bms_multi/sensor.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.const import CONF_ID
 from esphome.components import uart, sensor
 from esphome.const import UNIT_PERCENT, UNIT_VOLT, ICON_BATTERY
 
