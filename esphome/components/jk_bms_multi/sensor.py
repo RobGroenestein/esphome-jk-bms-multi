4fbbf06edc024a160892b17cdcdb96a91546df1a
RobGroenestein <rob@groenestein.eu>
2025-08-18 16:19:19 +0200

    Update sensor.py

--- esphome/components/jk_bms_multi/sensor.py
+++ esphome/components/jk_bms_multi/sensor.py
@@ -1,46 +1,50 @@
+# sensor.py - Aangepaste versie voor jk_bms_multi
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID
-from esphome.components import uart, sensor
-from esphome.const import UNIT_PERCENT, UNIT_VOLT, ICON_BATTERY
+from esphome.components import sensor, uart
+from esphome.const import CONF_ID, ICON_EMPTY, UNIT_EMPTY
 
 DEPENDENCIES = ["uart"]
+CODEOWNERS = ["@RobGroenestein"]
 
-# Namespace en class declaratie
-jk_ns = cg.esphome_ns.namespace("jk_bms_multi")
-JkBmsMulti = jk_ns.class_("JkBmsMulti", cg.Component, uart.UARTDevice)
+jk_bms_multi_ns = cg.esphome_ns.namespace("jk_bms_multi")
+JkBmsMultiComponent = jk_bms_multi_ns.class_("JkBmsMultiComponent", cg.Component)
 
-# Config schema
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(JkBmsMulti),   # <-- vereist 'id:' in YAML
-            cv.Required("soc"): cv.ensure_list(sensor.sensor_schema(
-                unit_of_measurement=UNIT_PERCENT,
-                icon=ICON_BATTERY,
-                accuracy_decimals=1,
-            )),
-            cv.Required("voltage"): cv.ensure_list(sensor.sensor_schema(
-                unit_of_measurement=UNIT_VOLT,
-                icon=ICON_BATTERY,
-                accuracy_decimals=2,
-            )),
-        }
-    ).extend(uart.UART_DEVICE_SCHEMA)
-)
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(JkBmsMultiComponent),
+        cv.Optional("uart_id"): cv.use_id(uart.UARTComponent),
+        cv.Optional("soc"): cv.ensure_list(sensor.sensor_schema(
+            unit_of_measurement="%",
+            accuracy_decimals=1,
+            icon=ICON_EMPTY,
+        )),
+        cv.Optional("voltage"): cv.ensure_list(sensor.sensor_schema(
+            unit_of_measurement="V",
+            accuracy_decimals=2,
+            icon=ICON_EMPTY,
+        )),
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
-# Vertaal YAML-config naar C++ code
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
 
-    # Koppel SoC sensoren
-    for i, conf in enumerate(config["soc"]):
-        sens = await sensor.new_sensor(conf)
-        var.soc_sensors[i] = sens
+    if "uart_id" in config:
+        uart_component = await cg.get_variable(config["uart_id"])
+        cg.add(var.set_uart_parent(uart_component))
 
-    # Koppel Voltage sensoren
-    for i, conf in enumerate(config["voltage"]):
-        sens = await sensor.new_sensor(conf)
-        var.voltage_sensors[i] = sens
+    if "soc" in config:
+        soc_sensors = []
+        for conf in config["soc"]:
+            soc_sensor = await sensor.new_sensor(conf)
+            soc_sensors.append(soc_sensor)
+        cg.add(var.set_soc_sensors(soc_sensors))
+
+    if "voltage" in config:
+        voltage_sensors = []
+        for conf in config["voltage"]:
+            voltage_sensor = await sensor.new_sensor(conf)
+            voltage_sensors.append(voltage_sensor)
+        cg.add(var.set_voltage_sensors(voltage_sensors))
